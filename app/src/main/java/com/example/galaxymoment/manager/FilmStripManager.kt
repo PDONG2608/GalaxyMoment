99fb85b1e293c59ad268eb404a2f151f633fc711
PDONG <phuongdongktmt1@gmail.com>
2024-12-08 23:00:51 +0700

    something vô nghĩa

--- app/src/main/java/com/example/galaxymoment/manager/FilmStripManager.kt
+++ app/src/main/java/com/example/galaxymoment/manager/FilmStripManager.kt
@@ -1,18 +1,23 @@
 package com.example.galaxymoment.manager
 
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.galaxymoment.adapter.FilmStripAdapter
 import com.example.galaxymoment.databinding.FragmentDetailBinding
 import com.example.galaxymoment.viewmodel.DetailViewModel
 
 
 class FilmStripManager(private val mDetailViewModel: DetailViewModel, private val binding: FragmentDetailBinding) {
+    private lateinit var snapHelper: LinearSnapHelper
     private lateinit var mLayoutManager: LinearLayoutManager
     private lateinit var mAdapterFilmStrip: FilmStripAdapter
+    private var oldPos: Int = 0
 
     init {
         initView()
+        filmStripListener()
     }
 
     private fun initView() {
@@ -20,7 +25,30 @@ class FilmStripManager(private val mDetailViewModel: DetailViewModel, private va
         mLayoutManager = LinearLayoutManager(mDetailViewModel.getContext(), LinearLayoutManager.HORIZONTAL, false)
         binding.filmStrip.adapter = mAdapterFilmStrip
         binding.filmStrip.layoutManager = mLayoutManager
-        val snapHelper = LinearSnapHelper()
+        binding.filmStrip.itemAnimator = null
+        snapHelper = LinearSnapHelper()
         snapHelper.attachToRecyclerView(binding.filmStrip)
+    }
+
+    private fun filmStripListener() {
+        binding.filmStrip.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val snapView = snapHelper.findSnapView(mLayoutManager)
+                val pos = mLayoutManager.getPosition(snapView!!)
+                if(oldPos != pos){
+                    mDetailViewModel.setCurrentPosFilmStrip(pos)
+                    oldPos = pos
+                }
+            }
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if(newState == RecyclerView.SCROLL_STATE_DRAGGING){
+                    mDetailViewModel.setFilmStripScrolling(true)
+                } else if (newState == RecyclerView.SCROLL_STATE_IDLE){
+                    mDetailViewModel.setFilmStripScrolling(false)
+                }
+            }
+        })
     }
 }
